e9746232980eaf18575808f6f87eb9be9e28e496
bigchewy <eric.w.page@gmail.com>
2016-03-07 20:46:45 -0800

    Added files via upload

---
+++ launcherSerialPort.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Pickles Treat Dispenser launcher
+#navigate to home directory, then to this directory then execute the python scripts then go back to home
+
+cd / 
+cd /home/pi
+#sudo python listenSerialPort.py
+cd / 
